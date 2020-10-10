660b7717ab3f826c7f8057866dc5fc48680aa873
Dennis Orsini <dennisorsini97@gmail.com>
2020-10-10 11:01:53 -0400

    update static site endpoint

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://kmodesto.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://vigilant-yonath-d9ccb5.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
